11db37bc1b2828b058092f228f460b17b689ad85
StevenvdWinkel <swinkel1@avans.nl>
2017-04-20 09:14:56 +0200

    My first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int sneeze = 8;
+		int counter = 0;
+		
+		while (counter < sneeze){
+			System.out.print("Ha");
+			counter = counter + 1;
+		}
+		System.out.println("choo Hello Steven");
+	}	
+}
